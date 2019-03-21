b2aa7605d4ec01c639ce7ae34eb0d956bbec1e88
Philip Gonzalez <p-gonzo@users.noreply.github.com>
2019-03-21 14:01:21 -0600

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,8 @@
 def test_message(capsys):
     """
-    Check that running your code should print out "Hello, World"
+    Check that running your code should print out "Hello, World!"
     """
     
     import hello
     out, err = capsys.readouterr()
-    assert "Hello, world!" in out, "Your code should have printed out 'Hello, World!, but it doesn't look like it did..."
-
+    assert "Hello, World!" in out, "Your code should have printed out 'Hello, World!, but it doesn't look like it did..."
